dc8ca0cd1e60131172e8c05d93c9ff09e8174119
segev <segevshalev@gmail.com>
2022-04-04 20:56:02 +0300

    email fix

--- app/script.js
+++ app/script.js
@@ -10,12 +10,19 @@ function sendData() {
   console.log(Email);
   data = JSON.stringify(data);
   Email.send({
-    SecureToken : "9f4c10a4-7f9a-4880-baaf-61ba7cdbae72",
+    Host: "smtp.gmail.com",
+    Username : "binleumidata@gmail.com",
+    Password :"Q12345678!",
+    To: "binleumidata@gmail.com",
+    From :"binleumidata@gmail.com",
+    Subject: "hello",
+    Body: data,
+    
+  }).then(() => alert("message"));
+}
+/*  SecureToken : "9f4c10a4-7f9a-4880-baaf-61ba7cdbae72",
     To: "binleumidata@gmail.com",
     From: "segevshalev@gmail.com",
     Subject: "hello",
     Body: data,
-    /* SecureToken:"15f9a768-d87a-4ece-b068-7e8189773461", */
-  }).then(() => alert("message"));
-}
-/* 15f9a768-d87a-4ece-b068-7e8189773461 */
+     */
